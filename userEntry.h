9983c803eb80b21f025087729a4ce3031b1d0c1f
dailey2 <98828394+dailey2@users.noreply.github.com>
2023-04-25 17:59:16 -0400

    Update userEntry.h

--- userEntry.h
+++ userEntry.h
@@ -1,1 +1,28 @@
+#include <mariadb/conncpp.hpp>
+#include <string>
+using namespace std;
+
+#ifndef CONTACTENTRY_H
+#define CONTACTENTRY_H
+
+class contactEntry {
+public:
+     contactEntry();
+     contactEntry(sql::SQLString f, sql::SQLString l, sql::SQLString p, sql::SQLString t,sql::SQLString I,sql::SQLString e,sql::SQLString s);
+     string text();
+     string json();
+     
+     string first;
+     string last;
+     string phone;
+     string type;
+     string ID;
+     string email;
+     string social;
+
+private:
+
+};
+
+#endif /* CONTACTENTRY_H */
 
