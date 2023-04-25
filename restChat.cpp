a1dd3d874b822b068274458bc3ba99656a855723
naf1965 <124084996+naf1965@users.noreply.github.com>
2023-04-25 18:44:11 -0400

    Update restChat.cpp

--- restChat.cpp
+++ restChat.cpp
@@ -112,7 +112,7 @@ svr.Get(R"(/chat/register/(.*)/(.*)/(.*))", [&](const Request& req, Response& re
     // Return the appropriate response
     string result;
     if (registrationSuccessful) {
-        UserDB.addEntry(username, email, password); // Call addEntry function here
+        userDB.addEntry(username, email, password); // Call addEntry function here
         result = "{\"status\":\"success\"}";
     } else {
         result = "{\"status\":\"fail\",\"error\":\"" + error + "\"}";
