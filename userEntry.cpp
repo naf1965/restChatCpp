1d5c2f0a39f23eca41850855ff2b3a21d5cdb370
dailey2 <98828394+dailey2@users.noreply.github.com>
2023-04-25 18:02:37 -0400

    Update userEntry.cpp

--- userEntry.cpp
+++ userEntry.cpp
@@ -1,1 +1,31 @@
+#include "userEntry.h"
+
+userEntry::userEntry() {
+}
+
+contactEntry::contactEntry(sql::SQLString n, sql::SQLString e, sql::SQLString p, sql::SQLString I) {
+    name = n;
+    email = e;
+    password = p;
+    ID=I;
+  
+}
+
+string userEntry::text() {
+	string result = ID + ". ";
+	result += name + " ";
+	result += email + " ";
+	result += password;
+	return result;
+
+}
+
+string contactEntry::json() {
+	string result = "{\"ID\":\"" + ID + "\",";
+	result += "\"name\":\"" + name + "\",";
+	result += "\"email\":\"" + email + "\",";
+	result += "\"password\":\"" + password + "\"}";
+	return result;
+
+}
 
